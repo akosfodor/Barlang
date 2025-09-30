f2e7c9e36b5827c11b1c8e6168c658ea449f4653
akosfodor <akosfodor10@gmail.com>
2025-09-30 08:58:46 +0200

    4. Feladat

---
+++ Pages/Delete.cshtml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Barlang.Data;
+using Barlang.Models;
+
+namespace Barlang.Pages
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Barlang.Data.BarlangDbContext _context;
+
+        public DeleteModel(Barlang.Data.BarlangDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public BarlangModel BarlangModel { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var barlangmodel = await _context.Barlangok.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (barlangmodel == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                BarlangModel = barlangmodel;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var barlangmodel = await _context.Barlangok.FindAsync(id);
+            if (barlangmodel != null)
+            {
+                BarlangModel = barlangmodel;
+                _context.Barlangok.Remove(BarlangModel);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Barlang.Data;
+using Barlang.Models;
 
 namespace Barlang.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly Barlang.Data.BarlangDbContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(Barlang.Data.BarlangDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        public IList<BarlangModel> BarlangModel { get;set; } = default!;
 
+        public async Task OnGetAsync()
+        {
+            BarlangModel = await _context.Barlangok.ToListAsync();
         }
     }
 }
